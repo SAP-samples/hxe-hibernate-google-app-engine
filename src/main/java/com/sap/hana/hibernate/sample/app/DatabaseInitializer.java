b3fafc48108a9a0a9c28ad493dfaaddaa3f9d7ff
Jonathan Bregler <jonathan.bregler@sap.com>
2018-06-12 10:04:06 +0200

    Add support for zipped CSV files.

--- src/main/java/com/sap/hana/hibernate/sample/app/DatabaseInitializer.java
+++ src/main/java/com/sap/hana/hibernate/sample/app/DatabaseInitializer.java
@@ -1,18 +1,29 @@
 package com.sap.hana.hibernate.sample.app;
 
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.SequenceInputStream;
+import java.io.UncheckedIOException;
 import java.net.URISyntaxException;
 import java.net.URL;
-import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.AbstractMap;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -79,73 +90,84 @@ public class DatabaseInitializer {
 						+ " does not exist. Please download the incidents data set as CSV from https://data.sfgov.org/Public-Safety/-Change-Notice-Police-Department-Incidents/tmnf-yvry" );
 			}
 			else {
-				Streams.mapWithIndex( Files.lines( Paths.get( incidentsCsvFileLocationUrl.toURI() ),
-						Charset.forName( "UTF-8" ) ).skip( 1 ), (line, idx) -> {
-							List<String> fields = parseLine( line );
-							assert fields.size() == 13;
+				Path incidentsCsvFileLocationPath = Paths.get( incidentsCsvFileLocationUrl.toURI() );
+				if ( this.incidentsCsvFileLocation.matches( ".+\\.zip(\\.001)?$" ) ) {
+					List<Path> extractedFiles = extractZip( incidentsCsvFileLocationPath );
+					if ( extractedFiles.size() != 1 ) {
+						throw new IllegalArgumentException( "The ZIP file must contain exactly one CSV file." );
+					}
+					incidentsCsvFileLocationPath = extractedFiles.get( 0 );
+				}
 
-							Incident i = new Incident( Long.parseLong( fields.get( 12 ) ) );
-							i.setIncidentNumber( fields.get( 0 ) );
-							i.setCategory( fields.get( 1 ) );
-							i.setDescription( fields.get( 2 ) );
-							i.setDayOfWeek( fields.get( 3 ) );
-							try {
-								i.setDate( SDF.parse( fields.get( 4 ) ) );
-							}
-							catch (ParseException e) {
-								log.info( "Unable to parse incident date: " + fields.get( 4 ), e );
-							}
-							i.setTime( fields.get( 5 ) );
-							i.setPdDistrict( fields.get( 6 ) );
-							i.setResolution( fields.get( 7 ) );
-							i.setAddress( fields.get( 8 ) );
-							try {
-								double x = Double.parseDouble( fields.get( 9 ) );
-								i.setX( x );
-							}
-							catch (NumberFormatException e) {
-								log.info( "Error parsing x coordinate", e );
-							}
-							try {
-								double y = Double.parseDouble( fields.get( 10 ) );
-								i.setY( y );
-							}
-							catch (NumberFormatException e) {
-								log.info( "Error parsing y coordinate", e );
-							}
+				try ( Stream<String> lines = Files.lines( incidentsCsvFileLocationPath, StandardCharsets.UTF_8 ) ) {
+					Long importedRecords = Streams.mapWithIndex( lines.skip( 1 ), (line, idx) -> {
+						if ( Thread.currentThread().isInterrupted() ) {
+							throw new RuntimeException( new InterruptedException( "The current thread has been interrupted" ) );
+						}
+						List<String> fields = parseLine( line );
+						assert fields.size() == 13;
 
-							Matcher matcher = LOCATION_PATTERN.matcher( fields.get( 11 ) );
-							if ( matcher.matches() ) {
-								if ( Double.isNaN( i.getX() ) ) {
-									i.setX( Double.parseDouble( matcher.group( 3 ) ) );
-								}
+						Incident i = new Incident( Long.parseLong( fields.get( 12 ) ) );
+						i.setIncidentNumber( fields.get( 0 ) );
+						i.setCategory( fields.get( 1 ) );
+						i.setDescription( fields.get( 2 ) );
+						i.setDayOfWeek( fields.get( 3 ) );
+						try {
+							i.setDate( SDF.parse( fields.get( 4 ) ) );
+						}
+						catch (ParseException e) {
+							log.info( "Unable to parse incident date: " + fields.get( 4 ), e );
+						}
+						i.setTime( fields.get( 5 ) );
+						i.setPdDistrict( fields.get( 6 ) );
+						i.setResolution( fields.get( 7 ) );
+						i.setAddress( fields.get( 8 ) );
+						try {
+							double x = Double.parseDouble( fields.get( 9 ) );
+							i.setX( x );
+						}
+						catch (NumberFormatException e) {
+							log.info( "Error parsing x coordinate", e );
+						}
+						try {
+							double y = Double.parseDouble( fields.get( 10 ) );
+							i.setY( y );
+						}
+						catch (NumberFormatException e) {
+							log.info( "Error parsing y coordinate", e );
+						}
 
-								if ( Double.isNaN( i.getY() ) ) {
-									i.setY( Double.parseDouble( matcher.group( 1 ) ) );
-								}
-							}
-							else {
-								log.warn( "Unable to parse location string " + fields.get( 11 ) );
-							}
-
-							if ( Double.isNaN( i.getX() ) || Double.isNaN( i.getY() ) ) {
-								log.warn( "The incident with ID " + i.getPdId() + " doesn't have a valid location." );
-								return new AbstractMap.SimpleEntry<>( idx, (Incident) null );
-							}
-
-							return new AbstractMap.SimpleEntry<>( idx, i );
-						} ).forEach( entry -> {
-							if ( entry.getValue() == null ) {
-								return;
+						Matcher matcher = LOCATION_PATTERN.matcher( fields.get( 11 ) );
+						if ( matcher.matches() ) {
+							if ( Double.isNaN( i.getX() ) ) {
+								i.setX( Double.parseDouble( matcher.group( 3 ) ) );
 							}
 
-							this.em.persist( entry.getValue() );
-
-							if ( entry.getKey() % this.jdbcBatchSize == 0 ) {
-								this.em.flush();
-								this.em.clear();
+							if ( Double.isNaN( i.getY() ) ) {
+								i.setY( Double.parseDouble( matcher.group( 1 ) ) );
 							}
-						} );
+						}
+						else {
+							log.warn( "Unable to parse location string " + fields.get( 11 ) );
+						}
+
+						if ( Double.isNaN( i.getX() ) || Double.isNaN( i.getY() ) ) {
+							log.warn( "The incident with ID " + i.getPdId() + " doesn't have a valid location." );
+							return Long.valueOf( 0 );
+						}
+
+						this.em.persist( i );
+
+						if ( idx % this.jdbcBatchSize == 0 ) {
+							this.em.flush();
+							this.em.clear();
+						}
+
+						return Long.valueOf( 1 );
+					} ).collect( Collectors.reducing( Long.valueOf( 0 ), e -> e, Long::sum ) );
+
+					log.info( "Imported " + importedRecords + " incidents." );
+				}
 			}
 		}
 		catch (IOException | URISyntaxException e) {
@@ -153,6 +175,97 @@ public class DatabaseInitializer {
 		}
 
 		log.info( "Importing incident sample data . . . ok" );
+	}
+
+	private List<Path> extractZip(Path csvFileLocationPath) throws IOException {
+		final String fileName = csvFileLocationPath.getFileName().toString();
+		final String fileLocationPrefix = fileName.substring( 0, fileName.lastIndexOf( '.' ) );
+		List<Path> extractedFiles = new ArrayList<>();
+		try ( Stream<Path> zipFiles = Files.find( csvFileLocationPath.getParent(), 1, (path, attributes) -> {
+			String pathFileName = path.getFileName().toString();
+			return attributes.isRegularFile() && pathFileName.startsWith( fileLocationPrefix )
+					&& pathFileName.matches( fileLocationPrefix + "\\.(zip|z[0-9]{2}|[0-9]{3})" );
+		} ).sorted( (path1, path2) -> {
+			if ( path1.getFileName().toString().endsWith( ".zip" ) ) {
+				return 1;
+			}
+			else if ( path2.getFileName().toString().endsWith( ".zip" ) ) {
+				return -1;
+			}
+			else {
+				return path1.compareTo( path2 );
+			}
+		} ) ) {
+			List<InputStream> pathList = null;
+			try {
+				pathList = zipFiles.map( path -> {
+					try {
+						InputStream inputStream = Files.newInputStream( path );
+						if ( path.getFileName().toString().endsWith( ".z01" ) ) {
+							// Skip an extension header on the first file if present
+							byte[] header = new byte[4];
+							inputStream.read( header );
+							long headerValue = 0 | header[0] | header[1] << 8 | header[2] << 16 | header[3] << 24;
+							if ( headerValue == 0x04034b50L ) {
+								// regular entry header => close the stream and return new stream starting from the
+								// beginning
+								inputStream.close();
+								inputStream = Files.newInputStream( path );
+							}
+						}
+						return inputStream;
+					}
+					catch (IOException e) {
+						throw new UncheckedIOException( e );
+					}
+				} ).collect( Collectors.toList() );
+
+				if ( pathList.isEmpty() ) {
+					throw new IllegalArgumentException( "No zip files found at " + csvFileLocationPath.getParent() );
+				}
+
+				Path tempDirectory = Files.createTempDirectory( "sfpd" );
+				try ( ZipInputStream zis = new ZipInputStream( new SequenceInputStream( Collections.enumeration( pathList ) ) ) ) {
+					ZipEntry entry = null;
+					while ( ( entry = zis.getNextEntry() ) != null ) {
+						if ( Thread.currentThread().isInterrupted() ) {
+							throw new RuntimeException( new InterruptedException( "The current thread has been interrupted" ) );
+						}
+						Path entryPath = Paths.get( entry.getName() );
+						if ( entryPath.isAbsolute() ) {
+							throw new IllegalArgumentException( "Absolute paths in ZIP files are not supported" );
+						}
+
+						Path entryAbsolutePath = tempDirectory.resolve( entryPath );
+						try ( OutputStream os = new BufferedOutputStream( new FileOutputStream( entryAbsolutePath.toFile() ) ) ) {
+							final int bufferSize = 1024;
+							byte[] buffer = new byte[bufferSize];
+							for ( int readBytes = -1; ( readBytes = zis.read( buffer, 0, bufferSize ) ) > -1; ) {
+								os.write( buffer, 0, readBytes );
+							}
+							os.flush();
+						}
+						extractedFiles.add( entryAbsolutePath );
+					}
+				}
+			}
+			finally {
+				if ( pathList != null ) {
+					pathList.forEach( is -> {
+						if ( is != null ) {
+							try {
+								is.close();
+							}
+							catch (IOException e) {
+								// ignore
+							}
+						}
+					} );
+				}
+			}
+		}
+
+		return extractedFiles;
 	}
 
 	/**
@@ -176,69 +289,81 @@ public class DatabaseInitializer {
 						+ " does not exist. Please download the addresses data set from https://data.sfgov.org/Geographic-Locations-and-Boundaries/Addresses-Enterprise-Addressing-System/sr5d-tnui" );
 			}
 			else {
-				Streams.mapWithIndex( Files.lines( Paths.get( addressesCsvFileLocationUrl.toURI() ),
-						Charset.forName( "UTF-8" ) ).skip( 1 ), (line, idx) -> {
-							List<String> fields = parseLine( line );
-							assert fields.size() == 11;
+				Path addressesCsvFileLocationPath = Paths.get( addressesCsvFileLocationUrl.toURI() );
+				if ( this.addressesCsvFileLocation.endsWith( ".zip" ) ) {
+					List<Path> extractedFiles = extractZip( addressesCsvFileLocationPath );
+					if ( extractedFiles.size() != 1 ) {
+						throw new IllegalArgumentException( "The ZIP file must contain exactly one CSV file." );
+					}
+					addressesCsvFileLocationPath = extractedFiles.get( 0 );
+				}
 
-							Address a = new Address( Long.parseLong( fields.get( 0 ) ) );
-							a.setCnn( fields.get( 1 ) );
-							a.setAddress( fields.get( 2 ) );
-							a.setAddressNumber( fields.get( 3 ) );
-							a.setAddressNumberSuffix( fields.get( 4 ) );
-							a.setStreetName( fields.get( 5 ) );
-							a.setStreetType( fields.get( 6 ) );
-							a.setZipCode( fields.get( 7 ) );
-							try {
-								double x = Double.parseDouble( fields.get( 8 ) );
-								a.setX( x );
-							}
-							catch (NumberFormatException e) {
-								log.info( "Error parsing x coordinate", e );
-							}
-							try {
-								double y = Double.parseDouble( fields.get( 9 ) );
-								a.setY( y );
-							}
-							catch (NumberFormatException e) {
-								log.info( "Error parsing y coordinate", e );
-							}
-							Matcher matcher = LOCATION_PATTERN.matcher( fields.get( 10 ) );
-							if ( matcher.matches() ) {
-								if ( Double.isNaN( a.getX() ) ) {
-									a.setX( Double.parseDouble( matcher.group( 3 ) ) );
-								}
+				try ( Stream<String> lines = Files.lines( addressesCsvFileLocationPath, StandardCharsets.UTF_8 ) ) {
+					Long importedRecords = Streams.mapWithIndex( lines.skip( 1 ), (line, idx) -> {
+						if ( Thread.currentThread().isInterrupted() ) {
+							throw new RuntimeException( new InterruptedException( "The current thread has been interrupted" ) );
+						}
+						List<String> fields = parseLine( line );
+						assert fields.size() == 11;
 
-								if ( Double.isNaN( a.getY() ) ) {
-									a.setY( Double.parseDouble( matcher.group( 1 ) ) );
-								}
-							}
-							else {
-								log.warn( "Unable to parse location string " + fields.get( 10 ) );
-							}
-
-							if ( Double.isNaN( a.getX() ) || Double.isNaN( a.getY() ) ) {
-								log.warn( "The address with ID " + a.getBaseID() + " doesn't have a valid location." );
-								return new AbstractMap.SimpleEntry<>( idx, (Address) null );
-							}
-
-							return new AbstractMap.SimpleEntry<>( idx, a );
-						} ).forEach( entry -> {
-							if ( entry.getValue() == null ) {
-								return;
+						Address a = new Address( Long.valueOf( fields.get( 0 ) ) );
+						a.setCnn( fields.get( 1 ) );
+						a.setAddress( fields.get( 2 ) );
+						a.setAddressNumber( fields.get( 3 ) );
+						a.setAddressNumberSuffix( fields.get( 4 ) );
+						a.setStreetName( fields.get( 5 ) );
+						a.setStreetType( fields.get( 6 ) );
+						a.setZipCode( fields.get( 7 ) );
+						try {
+							double x = Double.parseDouble( fields.get( 8 ) );
+							a.setX( x );
+						}
+						catch (NumberFormatException e) {
+							log.info( "Error parsing x coordinate", e );
+						}
+						try {
+							double y = Double.parseDouble( fields.get( 9 ) );
+							a.setY( y );
+						}
+						catch (NumberFormatException e) {
+							log.info( "Error parsing y coordinate", e );
+						}
+						Matcher matcher = LOCATION_PATTERN.matcher( fields.get( 10 ) );
+						if ( matcher.matches() ) {
+							if ( Double.isNaN( a.getX() ) ) {
+								a.setX( Double.parseDouble( matcher.group( 3 ) ) );
 							}
 
-							this.em.persist( entry.getValue() );
-
-							if ( entry.getKey() % this.jdbcBatchSize == 0 ) {
-								this.em.flush();
-								this.em.clear();
+							if ( Double.isNaN( a.getY() ) ) {
+								a.setY( Double.parseDouble( matcher.group( 1 ) ) );
 							}
-						} );
+						}
+						else {
+							log.warn( "Unable to parse location string " + fields.get( 10 ) );
+						}
+
+						if ( Double.isNaN( a.getX() ) || Double.isNaN( a.getY() ) ) {
+							log.warn( "The address with ID " + a.getBaseID() + " doesn't have a valid location." );
+							return Long.valueOf( 0 );
+						}
+
+						this.em.persist( a );
+
+						if ( idx % this.jdbcBatchSize == 0 ) {
+							this.em.flush();
+							this.em.clear();
+						}
+
+						return Long.valueOf( 1 );
+					} ).collect( Collectors.reducing( Long.valueOf( 0 ), e -> e, Long::sum ) );
+
+					log.info( "Imported " + importedRecords + " addresses." );
+				}
 			}
+
 		}
 		catch (IOException | URISyntaxException e) {
-			log.warn( "Error reading incidents file.", e );
+			log.warn( "Error reading addresses file.", e );
 		}
 
 		log.info( "Importing address sample data . . . ok" );
@@ -256,6 +381,9 @@ public class DatabaseInitializer {
 		StringBuilder currentField = new StringBuilder();
 
 		for ( char c : line.toCharArray() ) {
+			if ( Thread.currentThread().isInterrupted() ) {
+				throw new RuntimeException( new InterruptedException( "The current thread has been interrupted" ) );
+			}
 			switch ( c ) {
 				case '"':
 					quoted = !quoted;
